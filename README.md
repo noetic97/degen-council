0f141cd0193456f137802fd97392a7300549ee5a
jheitkamp <joe@joeheitkamp.com>
2023-01-11 23:10:04 -0700

    Add logo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # degen-council
+
 A Council of Degens
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./degenCouncil.gif";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>COMING SOON</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://discord.gg/7Sq6RMKmZ7"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You are pretty!
         </a>
       </header>
     </div>
